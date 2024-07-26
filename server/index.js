1c2ed3bebd7482789d62c33355253af7e8d79812
Raj Palival <rpalival@stevens.edu>
2024-07-26 15:53:59 -0400

    remove depreceted mongodb code from index.js

--- server/index.js
+++ server/index.js
@@ -57,14 +57,11 @@ app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    // Add this data only once
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-
-}).catch((error) => console.log(`${error} did not connect`))
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        // Add this data only once
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connect`))
