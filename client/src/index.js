d88768409a8b894c8c89778888e2abf23b87dd43
Raj Palival <rpalival@stevens.edu>
2024-07-30 02:20:32 -0400

    create store and configure store in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Redux Imports
+import authReducer from "./state";
+import { configureStore } from "react-redux";
+import { Provider } from 'react-redux';
+// Redux persist imports only if you are using redux-persist
+// Redux persist lets you store state data into local storage so that client can close his tabs
+// and still be able to access the data on re-login
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+            <App />
+        </PersistGate>
+    </Provider>
   </React.StrictMode>
-);
+)
