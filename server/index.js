492e78147541cd6128de9c6ea6c0c64ddeec9522
Raj Palival <rajpalival21@gmail.com>
2024-09-01 00:32:04 -0400

    updating .yml file

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ app.use("/posts", postRoutes);
 // PRODUCTION SETUP
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // MONGOOSE SETUP
